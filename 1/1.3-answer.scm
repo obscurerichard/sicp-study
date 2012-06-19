79669ea50efbc60965f32efaadb839f71696dd87
Richard Bullington-McGuire <richard@bullington-mcguire.com>
2012-06-19 15:25:44 -0400

    Clearly marked files for copyright / license status

--- 1/1.3-answer.scm
+++ 1/1.3-answer.scm
@@ -1,9 +1,13 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname sicp-1.3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+;;  Copyright © 1996, The Massachusetts Institute of Technology
+;;  Copyright © 2012, Richard Bullington-McGuire
+;;  Licensed under a 
+;;  Creative Commons Attribution-ShareAlike 3.0 Unported License
+;;  http://creativecommons.org/licenses/by-sa/3.0/
+;;
 (define (square x) (* x x))
 (define (ss2l a b c) (if (> a b)
                          (if (> b c) (square a b) (square a c))
-                         (if (> a c) (square b a) (square b c))
-                         _)
-  )
+                         (if (> a c) (square b a) (square b c))))
--- 1/1.6-answer.scm
+++ 1/1.6-answer.scm
@@ -1,9 +1,16 @@
 #lang scheme
-# The trouble with new-if is that its arguments are all going to be evaluated before
-# the function is called. This means that in this case the recursive call in the else clause
-# causes infinite recursion and an OOM error.
-#
-# 
+;;  Copyright © 1996, The Massachusetts Institute of Technology
+;;  Copyright © 2012, Richard Bullington-McGuire
+;;  Licensed under a 
+;;  Creative Commons Attribution-ShareAlike 3.0 Unported License
+;;  http://creativecommons.org/licenses/by-sa/3.0/
+;;
+;; The trouble with new-if is that its arguments are all going to be
+;; evaluated before the function is called. This means that in this 
+;; case the recursive call in the else clause causes infinite 
+;; recursion and an OOM error.
+
+
 (define (sqrt x)
   (define (square x) (* x x))
   (define (average x y) (/ (+ x y) 2))
